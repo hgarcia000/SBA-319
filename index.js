0a1bd9375f657d1ed4183e18aba25eba4a0716f9
Henry Garcia <hgarcia8889@gmail.com>
2025-06-20 14:52:11 -0400

    Established connection to MongoDB database.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import 'dotenv/config'
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+
+await mongoose.connect(process.env.MONGO_URI);
+console.log(`${Date()} : MongoDB Connected!`);
 
 app.get('/', (req, res) => {
     res.send('Welcome!');
